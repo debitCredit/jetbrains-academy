bf7b8989bf3e7267d2386c04044fdafe87a5c61c
Damian <debitCredit@users.noreply.github.com>
2021-02-24 17:22:31 +0100

    to clear stage 4/6, added transposition option, small refactoring

--- medium/Numeric Matrix Processor/processor.py
+++ medium/Numeric Matrix Processor/processor.py
@@ -26,11 +26,11 @@ class Matrix:
             if self.cols != other.rows:
                 print("The operation cannot be performed.")
             else:
-                empty = []
+                result = []
                 other_matrix_items = [[item[i] for item in other.matrix] for i in range(other.cols)]
                 for x, y in itertools.product(self.matrix, other_matrix_items):
-                    empty.append(sum(x_i * y_i for x_i, y_i in zip(x, y)))
-                return list(map(list, zip(*[iter(empty)] * other.cols)))
+                    result.append(sum(x_i * y_i for x_i, y_i in zip(x, y)))
+                return list(map(list, zip(*[iter(result)] * other.cols)))
 
     def __rmul__(self, other):
         return self.__mul__(other)
@@ -39,17 +39,35 @@ class Matrix:
         return self.__matmul__(other)
 
     def __str__(self):
-        # return "\n".join([" ".join(map('{:g}'.format, i)) for i in self.matrix])
+        # different string formatting if all values can be integers
         if all(x.is_integer() for x in itertools.chain.from_iterable(self.matrix)):
             return "\n".join([" ".join(map('{:.0f}'.format, i)) for i in self.matrix])
         else:
             return "\n".join([" ".join(map(str, i)) for i in self.matrix])
 
+    def transpose_main_diag(self):
+        return Matrix([*zip(*self.matrix)])
 
-PROMPT = "1. Add matrices\n" \
-         "2. Multiply matrix by a constant\n" \
-         "3. Multiply matrices\n" \
-         "0. Exit\n"
+    def transpose_side_diag(self):
+        return Matrix(list(reversed([*zip(*reversed(self.matrix))])))
+
+    def transpose_vertically(self):
+        return Matrix(list(item[::-1] for item in self.matrix))
+
+    def transpose_horizontally(self):
+        return Matrix(list(reversed(self.matrix)))
+
+
+PROMPT_MAIN = "1. Add matrices\n" \
+              "2. Multiply matrix by a constant\n" \
+              "3. Multiply matrices\n" \
+              "4. Transpose matrix\n" \
+              "0. Exit\n"
+
+PROMPT_TRANSPOSE = "1. Main diagonal\n" \
+                   "2. Side diagonal\n" \
+                   "3. Vertical line\n" \
+                   "4. Horizontal line\n"
 
 
 def init_matrix(n=""):
@@ -65,9 +83,9 @@ def main():
 
 
 def print_prompt():
-    print(PROMPT)
+    print(PROMPT_MAIN)
     choice = input("Your choice: ")
-    func_choices = {'1': add_matrices, '2': mult_matrix_constant, '3': mult_matrices, '0': shutdown}
+    func_choices = {'1': add_matrices, '2': mult_matrix_constant, '3': mult_matrices, '4': transpose, '0': shutdown}
     func_choices.get(choice, incorrect_input)()
 
 
@@ -100,6 +118,24 @@ def mult_matrices():
         print("The result is:")
         print(Matrix(result))
         print("")
+
+
+def transpose():
+    print(PROMPT_TRANSPOSE)
+    choice = input("Your choice: ")
+    func_choices = {'1': "transpose_main_diag",
+                    '2': "transpose_side_diag",
+                    '3': "transpose_vertically",
+                    '4': "transpose_horizontally"}
+    transpose_exec(func_choices.get(choice, incorrect_input))
+
+
+def transpose_exec(func):
+    matrix = Matrix(init_matrix())
+    print("The result is:")
+    result = getattr(matrix, func)()
+    print(result)
+    print("")
 
 
 def shutdown():
